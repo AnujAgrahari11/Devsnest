358d73f8796d7fce7c9a5718ee956f31b64684f1
AnujAgrahari11 <85186589+AnujAgrahari11@users.noreply.github.com>
2021-07-19 10:18:27 +0530

    app.jx

---
+++ frontend/Day16/memecard/src/App.jsx
@@ -0,0 +1,12 @@
+import "./style.css";
+
+function App() {
+	return (
+		<div className="Memecard">
+			<img src="https://i.redd.it/6obdsvltpz871.jpg" alt="" />
+			<h1>Meme Here</h1>
+			<p>Working Fine</p>
+		</div>
+	);
+}
+export default App;
